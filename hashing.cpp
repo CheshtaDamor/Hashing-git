db3413346335bdf47c309322043dfdf4a38fa5ab
CheshtaDamor <111421073+CheshtaDamor@users.noreply.github.com>
2022-08-17 17:35:13 +0530

    Update hashing.cpp

--- hashing.cpp
+++ hashing.cpp
@@ -2,4 +2,13 @@
 
 int hash_string(string s) {
     // implement hashing function here
+    int sum =0;
+    for(int i=0; i<s.length; i++){
+        if(s[i]!= ' '){
+            sum = sum + s[i];
+        }
+    }
+    
+    return sum%41;
+    
 }
